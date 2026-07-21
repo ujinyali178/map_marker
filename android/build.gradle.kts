77da4251a612f112d9d28221a37df70ee85be71c
root <root@localhost.localdomain>
2026-07-21 08:16:36 +0000

    fix: disable AAR metadata check for fluttertoast compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,15 +18,6 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            android {
-                compileSdk = 36
-            }
-        }
-    }
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
